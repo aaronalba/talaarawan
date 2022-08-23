475674b52460a31f5e2ceec2e6e05ac950b051d6
aaronalba <aaronalba.dev@gmail.com>
2022-08-23 08:50:15 +0800

    add DetailFragment

---
+++ app/src/main/java/com/aaron/talaarawan/ui/DetailFragment.kt
@@ -0,0 +1,35 @@
+package com.aaron.talaarawan.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.aaron.talaarawan.databinding.FragmentDetailBinding
+
+/**
+ * Shows the details of an entry.
+ */
+class DetailFragment : Fragment() {
+
+    /**
+     * View binding property used to access views. Valid only between
+     * onCreateView() and onDestroyView()
+     */
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
