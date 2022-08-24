06d9599fa15099d3b320e0069dadd2ded89118af
aaronalba <aaronalba.dev@gmail.com>
2022-08-24 10:45:19 +0800

    combine EditFragment into DetailFragment

--- app/src/main/java/com/aaron/talaarawan/ui/DetailFragment.kt
+++ app/src/main/java/com/aaron/talaarawan/ui/DetailFragment.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.aaron.talaarawan.data.getDayOfWeek
+import com.aaron.talaarawan.data.getFormattedDate
 import com.aaron.talaarawan.databinding.FragmentDetailBinding
+import com.aaron.talaarawan.viewmodels.JournalViewModel
+import com.aaron.talaarawan.viewmodels.JournalViewModelFactory
 
 /**
- * Shows the details of an entry.
+ * The screen for updating the values of an entry.
  */
 class DetailFragment : Fragment() {
+
+    private val viewModel: JournalViewModel by activityViewModels {
+        val application = requireActivity().application as JournalApplication
+        JournalViewModelFactory(
+            application.database.userDao(),
+            application.database.entryDao()
+        )
+    }
 
     /**
      * View binding property used to access views. Valid only between
@@ -19,6 +33,11 @@ class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +45,17 @@ class DetailFragment : Fragment() {
     ): View? {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // observe the selected entry from the viewModel and update UI appropriately
+        viewModel.entry.observe(viewLifecycleOwner) { entry ->
+            binding.detailDate.text = entry.getFormattedDate()
+            binding.detailDay.text = entry.getDayOfWeek()
+            binding.titleInputEdit.setText(entry.entryTitle)
+            binding.bodyInputEdit.setText(entry.entryBody)
+        }
     }
 
     override fun onDestroyView() {
